62fbcfe9d80956056f196677484795759c991a3e
big-of-big <60137763+big-of-big@users.noreply.github.com>
2022-01-10 07:41:29 +0900

    "a"

---
+++ deck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"スペード", "ダイア", "ハート", "クローバー"}
+	cardValues := []string{"エース", "2", "3", "4"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+"の"+value)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
