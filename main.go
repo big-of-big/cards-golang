d6a5e4f093cda497431852a935063133df23bb4a
big-of-big <60137763+big-of-big@users.noreply.github.com>
2022-01-11 09:50:06 +0900

    deal func

--- main.go
+++ main.go
@@ -2,5 +2,16 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+
+	//slice := []string{"a", "b", "c", "d", "e", "f", "g"}
+
+	// 頭から2つ
+	//fmt.Println(slice[0:2]) // [a b]
+	//fmt.Println(slice[:2])  // [a b]
+	// インデックス番号が2以上全て
+	//fmt.Println(slice[2:]) // [c d e f g]
 }
