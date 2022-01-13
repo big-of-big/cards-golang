32da34bc6c676bc86be50f9337434874780b7b61
big-of-big <60137763+big-of-big@users.noreply.github.com>
2022-01-14 06:59:15 +0900

    tostring

--- deck.go
+++ deck.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type deck []string
 
+// スペードからクローバーまでの1〜13の組み合わせの新しいデッキを作る
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"スペード", "ダイア", "ハート", "クローバー"}
@@ -23,6 +27,20 @@ func (d deck) print() {
 	}
 }
 
+// 52枚のカードからなるデッキを5枚と47枚のように分ける
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+	//slice := []string{"a", "b", "c", "d", "e", "f", "g"}
+
+	// 頭から2つ
+	//fmt.Println(slice[0:2]) // [a b]
+	//fmt.Println(slice[:2])  // [a b]
+	// インデックス番号が2以上全て
+	//fmt.Println(slice[2:]) // [c d e f g]
+}
+
+// deckを[]stringに型変換をして、要素をカンマ区切りで1つ文字列にする
+func (d deck) toString() string {
+	// deckと[]stringは互換性がある。[]string(deck)でdeckから[]stringに型変換できる。
+	return strings.Join([]string(d), ",")
 }
--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
-
-	//slice := []string{"a", "b", "c", "d", "e", "f", "g"}
-
-	// 頭から2つ
-	//fmt.Println(slice[0:2]) // [a b]
-	//fmt.Println(slice[:2])  // [a b]
-	// インデックス番号が2以上全て
-	//fmt.Println(slice[2:]) // [c d e f g]
+	fmt.Println(cards.toString())
 }
